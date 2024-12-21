53a4a8a80e3b81cecfe08ecf264f5c16af1741de
Howard Pinsley <hpinsley@gmail.com>
2024-12-21 22:32:48 +0000

    Clean up

--- elm-project/src/index.js
+++ elm-project/src/index.js
@@ -3,18 +3,11 @@
 // Require index.html so it gets copied to dist
 // let html = require('./index.html');
 import htmlString from './index.html';
-console.log(htmlString);
 const parser = new DOMParser();
 const root = parser.parseFromString(htmlString, 'text/html');
 document.documentElement.replaceWith(root.documentElement);
-// document.replaceChild(root.documentElement, document.documentElement);
 
 document.addEventListener('DOMContentLoaded', function () {
-    // const root = document.createElement("div");
-    // root.setAttribute("id", "main");
-    
-    // document.body.appendChild(root);
-
     var Elm = require('./Main.elm');
     var mountNode = document.getElementById('main');
 
