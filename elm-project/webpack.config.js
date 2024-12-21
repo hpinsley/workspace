b4e0e6be344f6f9469b4e9ef9f4858e69c0a2194
Howard Pinsley <hpinsley@gmail.com>
2024-12-21 18:28:23 +0000

    Seems to be able to build after loading installing the html loader

--- elm-project/webpack.config.js
+++ elm-project/webpack.config.js
@@ -2,12 +2,40 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: '/workspace/elm-project/index.html',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: {
+          loader: 'elm-webpack-loader',
+          options: {
+          },
+          options: {
+            files: ['Main.elm'],
+            cwd: '.',
+            optimize: false,
+            debug: true
+          }
+        }
+      }
+    ]
+  },
   devServer: {
-    contentBase: './dist'
+    contentBase: path.join(__dirname, '/workspace/elm-project/index.html'),
+    port: 8080
   }
+
+  // devServer: {
+  //   contentBase: './dist'
+  // }
 };
