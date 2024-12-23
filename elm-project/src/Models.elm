90c0e4776a33916fb72bfc2a9fc40faca0b349f6
Howard Pinsley <hpinsley@gmail.com>
2024-12-23 23:12:42 +0000

    Formatting

--- elm-project/src/Models.elm
+++ elm-project/src/Models.elm
@@ -8,32 +8,39 @@ type Msg
     | UpdateExpression String
     | ParseExpression
 
+
 type MulOp
     = Times
     | Divide
 
+
 type AddOp
     = Plus
     | Minus
+
 
 type Factor
     = IntFactor Int
     | FloatFactor Float
     | BinaryFactor MulOp Factor Factor
 
+
 type Expression
     = BinaryExpression Factor AddOp Factor
     | Factor
 
+
 type alias Model =
     { currentTime : Maybe Time.Posix
     , expression : Maybe String
-    , parsedExpression: Maybe Expression
+    , parsedExpression : Maybe Expression
     }
 
 
 init : flags -> ( Model, Cmd Msg )
 init _ =
-    let 
-        inital_model = { currentTime = Nothing, expression = Nothing, parsedExpression = Nothing    }
-    in (inital_model, Cmd.none)
+    let
+        inital_model =
+            { currentTime = Nothing, expression = Nothing, parsedExpression = Nothing }
+    in
+    ( inital_model, Cmd.none )
