9d1652ae7ee88ef434ad0f9e98a5efe5d1333a93
Howard Pinsley <hpinsley@gmail.com>
2024-12-24 21:21:23 +0000

    Show the variables with some styling

--- elm-project/src/Views.elm
+++ elm-project/src/Views.elm
@@ -21,13 +21,14 @@ showVariableList : Model -> Html Msg
 showVariableList model =
     let
         variables = Dict.values model.variables
-        trs = variables |> List.map (\v -> tr [] [td [][text v]])
+        trs = variables |> List.map (\v -> tr [] [td [][text v], td [][text "0"]])
     in
         div [id "variables"][
             table [][
                 thead [][
                     tr [][
-                        th [][text "Variable"]
+                          th [][text "Variable"]
+                        , th [][text "Value"]
                     ]
                 ]
                 , tbody [] trs
@@ -40,12 +41,11 @@ view model =
         [ id "adder" ]
         [ h1 [] [ text "Computations" ]
         , label [] [ text "Expression:" ]
-        , input [ onInput UpdateExpression ] []
+        , input [ id "expression-input", onInput UpdateExpression ] []
         , button [ class "command", onClick ParseExpression ] [ text "Parse" ]
+        , showVariableList model
+        , div [ id "parseErrors" ] [ text model.parseErrors ]
         , div [ id "time" ]
             [ getFormattedTime model.currentTime |> text
             ]
-        , div [] [ text (String.fromInt (String.length (Maybe.withDefault "" model.expression))) ]
-        , div [ id "parseErrors" ] [ text model.parseErrors ]
-        , showVariableList model
         ]
